93d728956b1e2b2ae466822c8c253afb22d7be75
hemantjava <hemantjava90@gmail.com>
2020-06-21 14:57:40 +0530

    Additional

--- src/main/java/com/example/configurationproperties/Credentials.java
+++ src/main/java/com/example/configurationproperties/Credentials.java
@@ -2,10 +2,13 @@ package com.example.configurationproperties;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class Credentials {
     private String authMethod;
     private String username;
     private String password;
+    private Map<String, String> additionalCredentials;
 
 }
